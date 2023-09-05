540283dffb81499bdd778681c1f53268fe66eda5
aslw1 <Abdelrahman.Eldahab@student.aast.edu>
2023-09-05 20:18:51 +0300

    b

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
 
   return (
     <section className='mt-36 mx-10 lg:mx-44'>
-        <h1 className='typewriter font-sans text-lg tracking-widest  lg:text-2xl xl:text-3xl'>Crafting Digital Experiences</h1>
+        <h1 className='typewriter font-sans text-lg tracking-widest p-1 lg:text-2xl xl:text-3xl'>Crafting Digital Experiences</h1>
         <blockquote className='mt-10 font-semibold md:w-[55%] lg:w-[55%] mb-6'>I'm Abdelrahman Basheer, a passionate software engineer  With a keen eye for design , I transform ideas into seamless digital solutions. Explore my projects and witness the fusion of creativity and technology that brings user experiences to life</blockquote>
         <a className='p-2 bg-black text-white md:p-2.5 rounded-full capitalize' href='#websites'>view my portfolio</a>
         <div ref={firstRef}  className={`pb- ${first ? "fade-in translate-y-2.5 ":""}`}>

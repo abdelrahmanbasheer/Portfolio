e890ac4d19f5d7b1059f0a6191cc0cd90375ce08
aslw1 <Abdelrahman.Eldahab@student.aast.edu>
2024-02-28 03:16:28 +0200

    added a footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div className=''>
+    <footer className=" text-white py-4">
+      <div className="container mx-auto text-center">
+        <p className="text-lg font-semibold">Abdelrahman Basheer ©</p>
+        <p className="text-sm">Built with React and Tailwind CSS 2024</p>
+      </div>
+    </footer>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Footer
+export default Footer;

29beec7ece279b2d54c0bf335d5d50d99e5fe88b
aslw1 <Abdelrahman.Eldahab@student.aast.edu>
2024-02-28 03:15:42 +0200

    added a condetional button for undeployed websites

--- components/CardButton.tsx
+++ components/CardButton.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { PostProps } from './types';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog';
 import Link from 'next/link';
+
 const CardButton = ({site}:PostProps) => {
   const para=site.description;
   const newPara = para
@@ -9,7 +10,7 @@ const CardButton = ({site}:PostProps) => {
       .map((str, index) => <p key={index}>{str}</p>);
   return (
     <Dialog>
-    <DialogTrigger className='m-2 p-2 bg-gray-500 opacity-90 hover:opacity-100 hover:scale-105 text-white rounded-full'>View</DialogTrigger>
+    <DialogTrigger className='m-2 p-2 w-[100px] bg-gray-500 transition-all hover:scale-105 ease-in-out delay-75 text-white rounded-full'>View</DialogTrigger>
     <DialogContent className='bg-white '>
       <DialogHeader>
         <DialogTitle className='text-center'>{site.title}</DialogTitle>
@@ -17,7 +18,11 @@ const CardButton = ({site}:PostProps) => {
          {newPara}
         </DialogDescription>
       </DialogHeader>
-      <Link href={`${site.slug}`} className='text-center bg-black p-2 text-white rounded-full opacity-[97%] hover:opacity-100' target='_blank'>visit the website</Link>
+     { site.title=="Formula AAST club" ?
+      <Link href={`${site.slug}`} className='text-center bg-black p-2 text-white rounded-full opacity-[97%] hover:opacity-100 capitalize' target='_blank'>deployment in progress</Link>
+      :
+      <Link href={`${site.slug}`} className='text-center bg-black p-2 text-white rounded-full opacity-[97%] hover:opacity-100 capitalize' target='_blank'>visit the website</Link>
+    }
     </DialogContent>
   </Dialog>
   )
